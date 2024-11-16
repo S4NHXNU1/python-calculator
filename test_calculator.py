54a8bdf68993f8323bc344a1c5fb070484152af7
S4NHXNU1 <sanhanut2003@gmail.com>
2024-11-16 17:05:04 +0700

    feat: add test cases + patch bug

--- test_calculator.py
+++ test_calculator.py
@@ -11,5 +11,25 @@ class TestCalculator(unittest.TestCase):
 
     # Add the following test methods to the TestCalculator class:
 
+    def test_ADD(self):
+        self.assertEqual(self.calc.add(4, 2), 6)
+        self.assertEqual(self.calc.add(5, 5), 10)
+
+    def test_SUB(self):
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(5, 5), 0)
+
+    def test_MUL(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(3, 3), 9)
+
+    def test_DIV(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+
+    def test_MOD(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+        self.assertEqual(self.calc.modulo(9, 3), 0)
+
 if __name__ == '__main__':
     unittest.main()
